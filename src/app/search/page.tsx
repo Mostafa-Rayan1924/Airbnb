5dd72a8ce5fbc831f3430ba2a67afa280854f338
Mostafa rayan <mr3309296@gmail.com>
2025-01-08 14:36:41 +0200

    finish project

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,13 @@
 import { getSearchData } from "@/api/app";
 import SearchCard from "@/components/SearchCard";
-import HeadSearch from "@/components/Sharable/HeadSearch";
+// import HeadSearch from "@/components/Sharable/HeadSearch";
 import { searchDataParent } from "@/components/types/types";
 const SearchPage = async () => {
   let data: searchDataParent = await getSearchData();
   return (
     <div className="my-[120px] w-[85%] m-auto  grid grid-cols-1 lg:grid-cols-2 gap-4 ">
       <div className="space-y-4">
-        <HeadSearch />
+        {/* <HeadSearch /> */}
         <div className="grid grid-cols-1 gap-4">
           {data.map((item) => {
             return <SearchCard item={item} />;
